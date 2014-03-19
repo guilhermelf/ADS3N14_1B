b0577f272b5661791aea66609f2084e0dbc1484f
Guilherme Longaray de Fraga <guilherme.lf@gmail.com>
2014-03-19 16:02:05 -0300

    Atualizando as estruturas de dados

--- AgendaTelefonica/src/com/senac/estruturas/ListaDuplamenteEncadeada.java
+++ AgendaTelefonica/src/com/senac/estruturas/ListaDuplamenteEncadeada.java
@@ -1,21 +1,33 @@
 package com.senac.estruturas;
 
 import static java.lang.System.out;
+import com.senac.modelos.Contato;
 
-public class ListaDuplamenteEncadeada<T> {
-	public Nodo<T> head;
-	public Nodo<T> tail;
+public class ListaDuplamenteEncadeada<T extends Comparable<T>> {
+	protected Nodo<T> head;
+	protected Nodo<T> tail;
 	
 	public ListaDuplamenteEncadeada() {
 		head = null;
 		tail = null;
 	}
 	
-	public void insert(Nodo<T> nodo) {
+	public Nodo<T> getFirst() {
+		return head;
+	}
+	
+	public Nodo<T> getLast() {
+		return tail;
+	}
+
+	public void insert(T dado) {
+		Nodo<T> nodo = new Nodo<T>(dado);
+		
 		if(head == null) 
 			head = nodo;
 		else {
 			nodo.setNext(head);
+			head.setPrev(nodo);
 			head = nodo;
 		}
 		
@@ -23,21 +35,36 @@ public class ListaDuplamenteEncadeada<T> {
 			tail = nodo;
 	}
 	
-	public void insert(Nodo<T> nodo, Nodo<T> anterior) {
-		nodo.setNext(anterior.getNext());
-		anterior.setNext(nodo);
-		nodo.setPrev(anterior);
-		
-		if(anterior == tail)
+	public void insert(T dado, Nodo<T> anterior) {
+		Nodo<T> nodo = new Nodo<T>(dado);
+		if (anterior != null) {
+			nodo.setNext(anterior.getNext());
+			nodo.setPrev(anterior);
+			anterior.getNext().setPrev(nodo);
+			anterior.setNext(nodo);
+			
+			if (anterior == tail)
+				tail = nodo;
+		} else {
+			if (tail != null) {
+				tail.setNext(nodo);
+				nodo.setPrev(tail);
+			} else {
+				head = nodo;
+			}
 			tail = nodo;
+		}
 	}
-	
-	public void append(Nodo<T> nodo) {
-		if(tail != null) {
+		
+	public void append(T dado) {
+		Nodo<T> nodo = new Nodo<T>(dado);
+		
+		if(tail == null) {
+			head = nodo;
+		} else {
 			tail.setNext(nodo);
 			nodo.setPrev(tail);
-		} else 
-			head = nodo;
+		}	
 		tail = nodo;
 	}
 	
@@ -47,5 +74,14 @@ public class ListaDuplamenteEncadeada<T> {
 			out.println(elem.getData());
 			elem = elem.getNext();
 		} while (elem != null);		
+	}
+	
+	public void printContato() {
+		Nodo<Contato> nodo = (Nodo<Contato>) head;
+		do {						
+			out.printf("Nome: %s\nTelefone: %d\n\n", nodo.getData().getNome(), nodo.getData().getTelefone());		
+			
+			nodo = nodo.getNext();
+		} while (nodo != null);				
 	}
 }
--- AgendaTelefonica/src/com/senac/estruturas/ListaOrdenada.java
+++ AgendaTelefonica/src/com/senac/estruturas/ListaOrdenada.java
@@ -1,5 +1,95 @@
 package com.senac.estruturas;
 
-public class ListaOrdenada {
+import static java.lang.System.out;
 
+import com.senac.modelos.Contato;
+
+public class ListaOrdenada<T extends Comparable<T>> {
+	protected Nodo<T> head;
+	protected Nodo<T> tail;
+	
+	public ListaOrdenada() {
+		this.head = null;
+		this.tail = null;
+	}
+	
+	public Nodo<T> getFirst() {
+		return head;
+	}
+	
+	public Nodo<T> getLast() {
+		return tail;
+	}
+	
+ 	public void insert(T dado) {
+		Nodo<T> nodo = new Nodo<T>(dado);
+		Nodo<T> percorrer = null;
+		Nodo<T> anterior = head;
+		
+		do {
+			if(anterior != null) {
+				if(anterior.getData().compareTo(nodo.getData()) == 0) {
+					percorrer = anterior;
+
+					if(percorrer.getPrev() != null)
+						percorrer.getPrev().setNext(nodo);
+					
+					nodo.setPrev(percorrer.getPrev());
+					percorrer.setNext(nodo);
+					nodo.setPrev(percorrer);	
+					
+					anterior = null;
+				} else if(anterior.getData().compareTo(nodo.getData()) > 0) {
+					percorrer = anterior;
+					
+					if(percorrer.getPrev() != null)
+						percorrer.getPrev().setNext(nodo);
+					
+					nodo.setPrev(percorrer.getPrev());
+					percorrer.setPrev(nodo);
+					nodo.setNext(percorrer);
+					
+										
+					if(percorrer == head)
+						head = nodo;
+					
+					anterior = null;
+				} else {
+					if(anterior.getNext() == null) {
+						nodo.setPrev(tail);
+						tail.setNext(nodo);		
+						tail = nodo;
+						
+						anterior = null;
+					} else
+						anterior = anterior.getNext();
+				}
+			
+				
+				
+			} else {
+					head = nodo;
+					tail = nodo;
+			}
+			
+		} while (anterior != null);
+	}
+
+	public void print() {
+		Nodo<T> elem = head;
+		do {
+			out.println(elem.getData());
+			elem = elem.getNext();
+		} while (elem != null);		
+	}
+	
+	public void printContato() {
+		Nodo<Contato> nodo = (Nodo<Contato>) head;
+		do {						
+			out.printf("Nome: %s\nTelefone: %d\n\n", nodo.getData().getNome(), nodo.getData().getTelefone());		
+			
+			nodo = nodo.getNext();
+		} while (nodo != null);				
+	}
+	
 }
