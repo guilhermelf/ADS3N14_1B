b639a80a438ed7bd1973cf32ac1dd561fcbe5896
Guilherme Longaray de Fraga <guilherme.lf@gmail.com>
2014-03-19 16:00:43 -0300

    Não há mais o atributo ddd

--- AgendaTelefonica/src/com/senac/modelos/Contato.java
+++ AgendaTelefonica/src/com/senac/modelos/Contato.java
@@ -1,19 +1,16 @@
 package com.senac.modelos;
 
-public class Contato {
+public class Contato implements Comparable<Contato> {
 	private String nome;
-	private Integer ddd;
 	private Integer telefone;
 	
 	public Contato() {
 		this.nome = null;
-		this.ddd = null;
 		this.telefone = null;
 	}
 	
-	public Contato(String nome, Integer ddd, Integer telefone) {
+	public Contato(String nome, Integer telefone) {
 		this.nome = nome;
-		this.ddd = ddd;
 		this.telefone = telefone;
 	}
 
@@ -25,19 +22,16 @@ public class Contato {
 		this.nome = nome;
 	}
 
-	public Integer getDdd() {
-		return ddd;
-	}
-
-	public void setDdd(Integer ddd) {
-		this.ddd = ddd;
-	}
-
 	public Integer getTelefone() {
 		return telefone;
 	}
 
 	public void setTelefone(Integer telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int compareTo(Contato contato) {
+		return this.getNome().compareToIgnoreCase(contato.getNome());
 	}
 }
