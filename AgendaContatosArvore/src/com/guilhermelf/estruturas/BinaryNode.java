8399f3cf6ffe340e78a691f6a200e6b91e8f2b5d
Guilherme Longaray de Fraga <guilherme.lf@gmail.com>
2014-04-26 09:53:48 -0300

    Criando o Nodo para a Arvore Binaria

---
+++ AgendaContatosArvore/src/com/guilhermelf/estruturas/BinaryNode.java
@@ -0,0 +1,37 @@
+package com.guilhermelf.estruturas;
+
+public class BinaryNode<T> {
+	private BinaryNode<T> left;
+	private BinaryNode<T> right;
+	private T dado;
+	
+	public BinaryNode(T dado) {
+		this.dado = dado;
+		this.left = null;
+		this.right = null;
+	}
+
+	public BinaryNode<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryNode<T> left) {
+		this.left = left;
+	}
+
+	public BinaryNode<T> getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryNode<T> right) {
+		this.right = right;
+	}
+
+	public T getDado() {
+		return dado;
+	}
+
+	public void setDado(T dado) {
+		this.dado = dado;
+	}
+}
